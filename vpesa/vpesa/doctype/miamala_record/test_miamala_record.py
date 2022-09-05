734f7fc23c1995fab394b48927fc70a59ea8aa2c
maftah <sirleemaftah@gmail.com>
2022-09-05 12:14:14 +0300

    Vpesa first commit

---
+++ vpesa/vpesa/doctype/miamala_record/test_miamala_record.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, KODEIT and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestMiamalaRecord(unittest.TestCase):
+	pass
---
+++ vpesa/vpesa/doctype/wakala_details/wakala_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, KODEIT and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Wakala Details', {
+	// refresh: function(frm) {
+
+	// }
+});
